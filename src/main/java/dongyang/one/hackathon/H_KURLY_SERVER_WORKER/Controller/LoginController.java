ff292bce7f40ec40b99881385690e4804adf089e
Hyun Woo <hwsa10041@gmail.com>
2022-08-14 02:03:23 +0900

    로그인 Controller 구현

---
+++ src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Controller/LoginController.java
@@ -0,0 +1,49 @@
+package dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Controller;
+
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Dto.WorkerLoginDto;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Jwt.JwtFilter;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Jwt.TokenProvider;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Repository.WorkerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/auth")
+public class LoginController {
+    private final WorkerRepository managerRepository;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authorize(@Valid @RequestBody WorkerLoginDto.Request request) {
+
+        // 토큰 생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(request.getUserId(), request.getPw());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+        return new ResponseEntity<>("good", httpHeaders, HttpStatus.OK);
+    }
+
+}
