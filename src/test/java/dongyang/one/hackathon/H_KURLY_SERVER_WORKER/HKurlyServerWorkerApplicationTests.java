bbf50be5b5ac0d02b0103e194b43e272215e41b8
Hyun Woo <hwsa10041@gmail.com>
2022-08-14 02:02:04 +0900

    메인 실행

---
+++ src/test/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/HKurlyServerWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package dongyang.one.hackathon.H_KURLY_SERVER_WORKER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HKurlyServerWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
