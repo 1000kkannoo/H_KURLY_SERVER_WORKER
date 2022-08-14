bd9b58b77afae5b47097dcfaec57daffd120fe43
Hyun Woo <hwsa10041@gmail.com>
2022-08-14 14:32:25 +0900

    회원가입 및 추가기능 구현을 위한 Repository 구현

---
+++ src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Repository/WorkerListRepository.java
@@ -0,0 +1,7 @@
+package dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Repository;
+
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.WorkerList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerListRepository extends JpaRepository<WorkerList,Long> {
+}
--- src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Repository/WorkerRepository.java
+++ src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Repository/WorkerRepository.java
@@ -4,10 +4,13 @@ import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.Worker;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
 
     @EntityGraph(attributePaths = "authorities")
     Optional<Worker> findOneWithAuthoritiesByUserId(String userId);
+
+    List<Worker> findByUserIdAndPnum(String userId, String pnum);
 }
