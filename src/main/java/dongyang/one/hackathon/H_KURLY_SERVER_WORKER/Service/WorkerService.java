1e2482777be3609193056fb157b6236bf406acd6
Hyun Woo <hwsa10041@gmail.com>
2022-08-14 14:32:39 +0900

    서비스 구현 및 테스트 완료

---
+++ src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Service/WorkerService.java
@@ -0,0 +1,169 @@
+package dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Service;
+
+
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Dto.TokenInfoResponseDto;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Dto.WorkerDto;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Dto.WorkerListDto;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Dto.WorkerRegisterDto;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.AuthorityWorker;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.Worker;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.WorkerList;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Exception.DuplicateManagerException;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Model.StatusTrue;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Repository.TokenRepository;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Repository.WorkerListRepository;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Repository.WorkerRepository;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.constant.Constable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Model.Model.AUTHORIZATION_HEADER;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class WorkerService {
+
+    private final WorkerListRepository workerListRepository;
+
+    private final TokenRepository tokenRepository;
+
+    private final WorkerRepository workerRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    // validate 및 단순 메소드화
+
+    private TokenInfoResponseDto getTokenInfo() {
+        return TokenInfoResponseDto.Response(
+                Objects.requireNonNull(SecurityUtil.getCurrentUsername()
+                        .flatMap(
+                                workerRepository::findOneWithAuthoritiesByUserId)
+                        .orElse(null))
+        );
+    }
+
+    private Boolean tokenCredEntialsValidate(HttpServletRequest request) {
+        String getToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (!tokenRepository.existsById(getToken)) {
+            return false;
+        }
+        return true;
+    }
+
+    // Service
+
+    // 회원가입
+    @Transactional
+    public Constable registerWorker(WorkerRegisterDto.Request request) {
+        if (workerRepository.findOneWithAuthoritiesByUserId(request.getUserId()).orElse(null) != null) {
+            throw new DuplicateManagerException("이미 가입되어 있는 유저입니다.");
+        }
+        AuthorityWorker authority = AuthorityWorker.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        // 근무자 정보 등록
+        workerRepository.save(
+                Worker.builder()
+                        .userId(request.getUserId())
+                        .pw(passwordEncoder.encode(request.getPw()))
+                        .name(request.getName())
+                        .pnum(request.getPnum())
+                        .gender(request.getGender())
+                        .hnum(request.getHnum())
+                        .authorities(Collections.singleton(authority))
+                        .build()
+        );
+
+        // 근무자 인력현황 정보 등록
+        workerListRepository.save(
+                WorkerList.builder()
+                        .con(false)
+                        .edu(false)
+                        .arrangement('N')
+                        .wscore(100)
+                        .wcnt(0)
+                        .name(request.getName())
+                        .pnum(request.getPnum())
+                        .build()
+        );
+
+        return StatusTrue.REGISTER_STATUS_TRUE;
+    }
+
+    // 회원정보수정을 위한 근무자 인력현황 정보 가져오기
+    @Transactional
+    public List<Object> getUpdateWorker() {
+        Long a = getTokenInfo().getId();
+        log.info(String.valueOf(a));
+        return workerListRepository
+                .findById(getTokenInfo().getId())
+                .stream()
+                .map(WorkerListDto::Response)
+                .collect(Collectors.toList());
+    }
+
+    // 근무자 인력현황 정보 및 근무자 정보 업데이트
+    @Transactional
+    public Constable postUpdateWorker(WorkerListDto.updateRequest request) {
+        AuthorityWorker authority = AuthorityWorker.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        log.info(getTokenInfo().getPw());
+
+        // 근무자 정보 업데이트
+        workerRepository.save(
+                Worker.builder()
+                        .id(getTokenInfo().getId())
+                        .userId(getTokenInfo().getUserId())
+                        .pw(getTokenInfo().getPw())
+                        .name(getTokenInfo().getName())
+                        .pnum(request.getPnum())
+                        .gender(getTokenInfo().getGender())
+                        .hnum(getTokenInfo().getHnum())
+                        .authorities(Collections.singleton(authority))
+                        .build()
+        );
+
+        // 근무자 인력현황 정보 업데이트
+        workerListRepository.save(
+                WorkerList.builder()
+                        .id(getTokenInfo().getId())
+                        .workDay(request.getWorkDay())
+                        .workPlace(request.getWorkPlace())
+                        .workTime(request.getWorkTime())
+                        .workType(request.getWorkType())
+                        .con(request.getCon())
+                        .edu(request.getEdu())
+                        .arrangement(request.getArrangement())
+                        .wscore(request.getWscore())
+                        .wcnt(request.getWcnt())
+                        .name(getTokenInfo().getName())
+                        .pnum(request.getPnum())
+                        .build()
+        );
+
+        return StatusTrue.UPDATE_STATUS_TRUE;
+    }
+
+    // id 찾기
+    @Transactional
+    public List<Object> idSearch(WorkerDto.idSearchRequest request) {
+        return workerRepository.findByUserIdAndPnum(request.getUserId(),request.getPnum())
+                .stream()
+                .map(WorkerDto.idSearchRepsonse::repsonse)
+                .collect(Collectors.toList());
+    }
+}
