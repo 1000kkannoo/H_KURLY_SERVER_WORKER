483ec750a458cc23dfc7c93bf86a07e8a788a46b
Hyun Woo <hwsa10041@gmail.com>
2022-08-15 14:16:56 +0900

    비밀번호 변경을 위한 Dto 구현

--- src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Dto/WorkerDto.java
+++ src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Dto/WorkerDto.java
@@ -4,6 +4,9 @@ import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.Worker;
 import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.WorkerList;
 import lombok.*;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 public class WorkerDto {
@@ -32,9 +35,15 @@ public class WorkerDto {
     @NoArgsConstructor
     @Builder
     public static class pwChangeRequest{
+        private Long id;
+        private String name;
+        private String pnum;
         private String userId;
         private String pw;
+        private String oldPw;
         private String newPw;
+        private Character gender;
+        private String hnum;
     }
 
     @Getter
@@ -58,10 +67,22 @@ public class WorkerDto {
     @NoArgsConstructor
     @Builder
     public static class pwResponse{
+        private Long id;
+        private String name;
+        private String pnum;
+        private String userId;
         private String pw;
+        private Character gender;
+        private String hnum;
         public static WorkerDto.pwResponse pwResponse (@NotNull Worker worker){
             return pwResponse.builder()
+                    .id(worker.getId())
+                    .name(worker.getName())
+                    .pnum(worker.getPnum())
+                    .userId(worker.getUserId())
                     .pw(worker.getPw())
+                    .gender(worker.getGender())
+                    .hnum(worker.getHnum())
                     .build();
         }
     }
