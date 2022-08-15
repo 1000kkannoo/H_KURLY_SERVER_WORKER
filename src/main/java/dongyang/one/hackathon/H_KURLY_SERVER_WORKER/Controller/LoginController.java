06ae318096210294261213ca407b09c37cf85e19
Hyun Woo <hwsa10041@gmail.com>
2022-08-15 15:37:04 +0900

    로그아웃 기능 구현을 위한 Controller 수정
    토큰을 DB에 저장

--- src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Controller/LoginController.java
+++ src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Controller/LoginController.java
@@ -1,9 +1,11 @@
 package dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Controller;
 
 import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Dto.WorkerLoginDto;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.workerToken;
 import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Jwt.JwtFilter;
 import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Jwt.TokenProvider;
 import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Model.StatusTrue;
+import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Repository.TokenRepository;
 import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Repository.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +29,7 @@ import javax.validation.Valid;
 @RequestMapping(value = "/auth")
 @Slf4j
 public class LoginController {
-    private final WorkerRepository managerRepository;
+    private final TokenRepository tokenRepository;
     private final TokenProvider tokenProvider;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
@@ -43,6 +45,11 @@ public class LoginController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String jwt = tokenProvider.createToken(authentication);
+
+        // 토큰 유효성 검증을 위한 데이터 저장 (로그아웃을 위한 장치)
+        tokenRepository.save(workerToken.builder()
+                .token("Bearer " + jwt)
+                .build());
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
