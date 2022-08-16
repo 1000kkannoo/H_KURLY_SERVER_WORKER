5f1454b85b87e58299e573a33b2a4306295fc8ec
Hyun Woo <hwsa10041@gmail.com>
2022-08-16 15:52:41 +0900

    출퇴근 기록을 위한 RecordRepository 생성

--- src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Repository/RecordRepository.java
+++ src/main/java/dongyang/one/hackathon/H_KURLY_SERVER_WORKER/Repository/RecordRepository.java
@@ -3,5 +3,9 @@ package dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Repository;
 import dongyang.one.hackathon.H_KURLY_SERVER_WORKER.Entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RecordRepository extends JpaRepository<Record,Long> {
+    Optional<Record> findByIdxAndWcnt(Long idx, Integer wcnt);
 }
